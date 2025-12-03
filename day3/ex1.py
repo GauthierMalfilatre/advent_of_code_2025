58184cac6330f32edd8eb6028ed26e18bdf17de5
Gauthier MALFILATRE <gauthier.malfilatre@epitech.eu>
2025-12-03 09:44:58 +0100

    [ADD] Add day3 (2 gold star)

---
+++ day3/ex1.py
@@ -0,0 +1,44 @@
+## Advent of Code day3 : Ex1 - Gauthier Malfilatre
+
+def get_file_lines(filepath: str) -> list[str]:
+    """ Return a file lines in a list of str """
+    lines: list[str] = []
+    try:
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+    except:
+        print(f"Cannot read \"{filepath}\".")
+    return lines
+
+def biggest_digit(line: str) -> tuple[int, int]:
+    """ Find the biggest digit in a str fullfill of digits and its index """
+    biggest: int = 0
+    bindex : int = -1
+    for i, c in enumerate(line):
+        if c != '\n' and int(c) > int(biggest):
+            biggest = int(c)
+            bindex = i
+    return bindex, int(biggest)
+
+def biggest_number(line: str, r: int = 2) -> int:
+    """ Find the biggest number contained in a str fullfill of int """
+    found: str = ""
+    index: int = -1
+    for i in range(r):
+        index, digit = biggest_digit(line[index + 1:len(line) - (r - i)])
+        if index == -1:
+            print("Error while parsing")
+        found += str(digit)
+    print(found)
+    return int(found)
+
+def sum_of_banks(lines: list[str], r: int = 2) -> int:
+    """ Find the sum of the biggest joltage of all banks """
+    summup: int = 0
+    for bank in lines:
+        summup += biggest_number(bank, r)
+    return summup
+
+if __name__ == "__main__":
+    result: int = sum_of_banks(get_file_lines("day3/input.txt"), 2)
+    print(result)
