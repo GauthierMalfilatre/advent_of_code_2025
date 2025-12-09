18046fb141578fd6b81c520ecfc8fa655ecb8c78
Gauthier MALFILATRE <gauthier.malfilatre@epitech.eu>
2025-12-09 15:16:27 +0100

    [ADD] Try to make day9ex2.

--- day9/ex2.py
+++ day9/ex2.py
@@ -10,7 +10,6 @@ def get_file_lines(filepath: str) -> list[str]:
         print(f"Cannot read \"{filepath}\".")
     return lines
 
-
 def fill_with_green(map_: list[list[int]], lines: list[tuple[int, int]]) -> None:
     """ Fill with green dots """
     for i, tile in enumerate(lines):
@@ -34,14 +33,14 @@ def generate_map(lines: list[tuple[int]]) -> list[list[int]]:
         for x in range(xmax + 2):
             map_[y].append('.')
     for i in lines:
-        map_[i[1]][i[0]] = '#'
-    fill_with_green(map_, lines)
+        map_[i[1]][i[0]] = i[2]
+#    fill_with_green(map_, lines)
     return map_
 
 def get_area(p1: tuple[int, int], p2: tuple[int, int]) -> int:
     """ Get the area of the rectangle """
-    x1, y1 = p1
-    x2, y2 = p2
+    x1, y1, t = p1
+    x2, y2, t = p2
     return (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
 
 def is_valid(lines: list[tuple[int, int]], x: int, y: int) -> bool:
@@ -65,26 +64,87 @@ def is_valid(lines: list[tuple[int, int]], x: int, y: int) -> bool:
             return False
     return True
 
+def is_valid2(lines: list[tuple[int, int]], x: int, y: int) -> bool:
+    """ is valid 2 (new approch) """
+    a: tuple[int] = lines[x]
+    b: tuple[int] = (lines[x][0], lines[y][1], '#')
+    c: tuple[int] = lines[y]
+    d: tuple[int] = (lines[y][0], lines[x][1], '#')
+    for p in (a, b, c, d):
+        xfounds = []
+        yfounds = []
+        xcount: int = 0
+        ycount: int = 0
+        xcount_: int = 0
+        ycount_: int = 0
+        for i in lines:
+            if i[0] < p[0] and i[0] not in xfounds:
+#                print("P1", i[0], p[0])
+                xcount += 1
+                xfounds.append(i[0])
+            if i[1] < p[1] and i[1] not in yfounds:
+                ycount += 1
+#                print("P1", i[1], p[1])
+                yfounds.append(i[1])
+            if i[0] > p[0] and i[0] not in xfounds:
+#                print("P1", i[0], p[0])
+                xcount_ += 1
+                xfounds.append(i[0])
+            if i[1] > p[1] and i[1] not in yfounds:
+                ycount_ += 1
+#                print("P1", i[1], p[1])
+                yfounds.append(i[1])
+#        print(p, xcount, ycount, xcount_, ycount_)
+        if not p in lines and (xcount % 2 != 0 or ycount % 2 != 1):
+            return False
+        if not p in lines and (xcount_ % 2 != 1 or ycount_ % 2 != 0 or ycount_ < 1 or xcount_ < 1):
+            return False
+    return True
+
 def find_bsq(lines: list[tuple[int, int]]) -> int:
     """ Find the biggest rectangle """
     indexs: list[tuple[int, int]] = None
     area  : int                   = None
     for i, tile in enumerate(lines):
+        if tile[2] != '#':
+            continue
         for j, tile_ in enumerate(lines):
-            if j == i:
+            if j == i or tile_[2] != '#':
                 continue
             temp = get_area(tile, tile_)
-            if (area == None or temp > area) and is_valid(lines, i, j):
+            if (area == None or temp > area) and is_valid2(lines, i, j):
                 area = temp
                 indexs = (i, j)
     return area, indexs
 
+def add_green_tiles(lines: list[tuple[int]]) -> None:
+    """ Fill with green tiles """
+    i = 0
+    lines.append(lines[0])
+    while i < len(lines) - 1:
+        if lines[i][2] == '#':
+            if lines[i][0] == lines[i + 1][0]:
+                a, b = lines[i][1], lines[i + 1][1]
+                if lines[i][1] > lines[i + 1][1]:
+                    a, b = b, a
+                for j in range(a + 1, b):
+                    lines.insert(i + 1, (lines[i][0], j, 'X'))
+                    i += 1
+            if lines[i][1] == lines[i + 1][1]:
+                a, b = lines[i][0], lines[i + 1][0]
+                if lines[i][0] > lines[i + 1][0]:
+                    a, b = b, a
+                for j in range(a + 1, b):
+                    lines.insert(i + 1, (j, lines[i][1], 'X'))
+                    i += 1
+        i += 1
+
 if __name__ == "__main__":
     lines: list[str] = get_file_lines("day9/input.txt")
-    lines = [tuple([int(i) for i in j[:-1].split(',')]) for j in lines]
+    lines = [tuple([int(i) for i in j[:-1].split(',')]+['#',]) for j in lines]
+    # add_green_tiles(lines)
     result, indexs = find_bsq(lines)
-    
-    print(lines[indexs[0]], lines[indexs[1]])
+    """
     map_ = generate_map(lines)
     map_[lines[indexs[0]][1]][lines[indexs[0]][0]] = '0'
     map_[lines[indexs[1]][1]][lines[indexs[1]][0]] = '0'
@@ -92,4 +152,6 @@ if __name__ == "__main__":
         for j in i:
             print(j, end="")
         print("\n", end="")
+    is_valid2(lines, indexs[0], indexs[1])
+    """
     print(result)
